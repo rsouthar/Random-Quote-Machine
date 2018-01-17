05684148336ed703e4563271fd9f64d2815381ba
Rick <rdsouthard1979@gmail.com>
2018-01-16 21:25:05 -0600

    added some real quotes

--- quotes.js
+++ quotes.js
@@ -1,6 +1,8 @@
 var quotes = [
-  "Howdy",
-  "hello"
+  "Knowledge will give you power, but character respect. -Bruce Lee",
+  "Do, or do not.  There is no try. -Yoda",
+  "In any contest between power and patience, bet on patience. -W.B. Prescott",
+  "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin, but by the content of their character. - Martin Luther King, Jr."
 ];
 
 function nextQuote() {
