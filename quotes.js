fa8398c61f7655742615889f72997882a6b4e48d
Rick <rdsouthard1979@gmail.com>
2018-01-19 05:37:27 -0600

    Added more quotes and played with style

--- quotes.js
+++ quotes.js
@@ -2,7 +2,13 @@ var quotes = [
   "Knowledge will give you power, but character respect. -Bruce Lee",
   "Do, or do not.  There is no try. -Yoda",
   "In any contest between power and patience, bet on patience. -W.B. Prescott",
-  "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin, but by the content of their character. - Martin Luther King, Jr."
+  "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin, but by the content of their character. - Martin Luther King, Jr.",
+  "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts. -Bertrand Russell",
+  "If a man does his best, what else is there? -General George S. Patton",
+  "We didn't lose the game; we just ran out of time. -Vinve Lombardi",
+  "You can only find truth with logic if you have already found truth without it. -Gilbert Keith Chesterton",
+  "A people that values its privileges above its principles soon loses both. -Dwight D. Eisenhower",
+  "Sleep is an excellent way of listening to an opera. -James Stephens"
 ];
 
 function nextQuote() {
